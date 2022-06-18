d03d599be20751f3f45b22831ea52375dbe48461
s22442 <s22442@pjwstk.edu.pl>
2022-06-18 11:52:41 +0200

    init

---
+++ src/main/java/com/pjatk/jazs22442nbp/JazS22442NbpApplication.java
@@ -0,0 +1,13 @@
+package com.pjatk.jazs22442nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS22442NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS22442NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjatk/jazs22442nbp/JazS22442NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjatk.jazs22442nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS22442NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
