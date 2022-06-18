9e805f40b59c5835111bb433672c63a727cf69cd
s22442 <s22442@pjwstk.edu.pl>
2022-06-18 13:57:03 +0200

    docs: add missing GoldRate descriptions

--- src/main/java/com/pjatk/jazs22442nbp/goldrate/model/GoldRate.java
+++ src/main/java/com/pjatk/jazs22442nbp/goldrate/model/GoldRate.java
@@ -1,7 +1,14 @@
 package com.pjatk.jazs22442nbp.goldrate.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "NBP Gold Rate", description = "Gold rate data with value and date from NBP API")
 public class GoldRate {
+    @ApiModelProperty(value = "The value of the gold rate")
     private double cena;
+
+    @ApiModelProperty(value = "The date of the gold rate")
     private String data;
 
     public GoldRate() {
